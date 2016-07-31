f308980229239d5a9b1befd064f78c8f37bf771b
maxotek <partho@maxotek.com>
2016-07-31 06:12:45 +0530

    First commit of new ASP .NET Project

---
+++ vidly-aspnet-learn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vidly_aspnet_learn.MvcApplication" Language="C#" %>
---
+++ vidly-aspnet-learn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidly_aspnet_learn.Startup))]
+namespace vidly_aspnet_learn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
