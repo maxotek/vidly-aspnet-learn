672be047b7ad7c19b18f7fd0383ce21ca7d6783c
maxotek <partho@maxotek.com>
2016-07-31 06:21:58 +0530

    Added MoviesController with Random Action

---
+++ vidly-aspnet-learn/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿#region Copyright
+
+// Maxotek CONFIDENTIAL INFORMATION
+// © 2007-2016 Maxotek Inc.
+// All Rights Reserved
+//                                                                   
+// This program contains confidential and proprietary information   
+// of the Maxotek, Inc.  Any reproduction, disclosure, or use       
+// in whole or in part is expressly prohibited, except as may be    
+// specifically authorized by prior written agreement.
+
+#endregion
+
+#region Imports
+
+using System.Web.Mvc;
+using vidly_aspnet_learn.Models;
+
+#endregion
+
+namespace vidly_aspnet_learn.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Name = "Shrek!"
+            };
+            return View(movie);
+        }
+    }
+}
---
+++ vidly-aspnet-learn/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿#region Copyright
+
+// Maxotek CONFIDENTIAL INFORMATION
+// © 2007-2016 Maxotek Inc.
+// All Rights Reserved
+//                                                                   
+// This program contains confidential and proprietary information   
+// of the Maxotek, Inc.  Any reproduction, disclosure, or use       
+// in whole or in part is expressly prohibited, except as may be    
+// specifically authorized by prior written agreement.
+
+#endregion
+
+namespace vidly_aspnet_learn.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
