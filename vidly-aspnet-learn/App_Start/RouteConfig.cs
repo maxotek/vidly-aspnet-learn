fd895d99f881ff72637225492fd5be3b71c9b0ed
maxotek <partho@maxotek.com>
2016-07-31 06:40:55 +0530

    Converted custom route to attribute based

--- vidly-aspnet-learn/App_Start/RouteConfig.cs
+++ vidly-aspnet-learn/App_Start/RouteConfig.cs
@@ -25,11 +25,7 @@ namespace vidly_aspnet_learn
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute("MoviesByReleaseDate",
-                "movies/released/{year}/{month}",
-                new {controller = "Movies", action = "ByReleaseDate"},
-                new {year = "2015|2016", month = @"\d{2}"});
+            routes.MapMvcAttributeRoutes();
 
             routes.MapRoute(
                 "Default",
--- vidly-aspnet-learn/Controllers/MoviesController.cs
+++ vidly-aspnet-learn/Controllers/MoviesController.cs
@@ -48,6 +48,7 @@ namespace vidly_aspnet_learn.Controllers
             return Content(string.Format("pageIndex={0}, sortBy={1}", pageIndex, sortBy));
         }
 
+        [Route("movies/released/{year:range(2015,2016)}/{month:regex(\\d{2}):range(1,12)}")]
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content(year + "/" + month);
