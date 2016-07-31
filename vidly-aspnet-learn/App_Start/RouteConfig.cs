18f2368a6748c8a145b6cef28aa7e65d3e4b461f
maxotek <partho@maxotek.com>
2016-07-31 06:29:37 +0530

    Added ByReleaseDate action to MoviesController to demo custom routes with constraints

--- vidly-aspnet-learn/App_Start/RouteConfig.cs
+++ vidly-aspnet-learn/App_Start/RouteConfig.cs
@@ -26,6 +26,11 @@ namespace vidly_aspnet_learn
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new {controller = "Movies", action = "ByReleaseDate"},
+                new {year = "2015|2016", month = @"\d{2}"});
+
             routes.MapRoute(
                 "Default",
                 "{controller}/{action}/{id}",
--- vidly-aspnet-learn/Controllers/MoviesController.cs
+++ vidly-aspnet-learn/Controllers/MoviesController.cs
@@ -47,5 +47,10 @@ namespace vidly_aspnet_learn.Controllers
 
             return Content(string.Format("pageIndex={0}, sortBy={1}", pageIndex, sortBy));
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
