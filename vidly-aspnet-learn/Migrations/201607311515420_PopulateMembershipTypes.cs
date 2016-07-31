0620d75406bbda78f4a9702ccbdf61f3aafe9c06
maxotek <partho@maxotek.com>
2016-07-31 20:51:03 +0530

    Seeded MembershipTypes into database

---
+++ vidly-aspnet-learn/Migrations/201607311515420_PopulateMembershipTypes.cs
@@ -0,0 +1,36 @@
+#region Copyright
+
+// Maxotek CONFIDENTIAL INFORMATION
+// © 2007-2016 Maxotek Inc.
+// All Rights Reserved
+//                                                                   
+// This program contains confidential and proprietary information   
+// of the Maxotek, Inc.  Any reproduction, disclosure, or use       
+// in whole or in part is expressly prohibited, except as may be    
+// specifically authorized by prior written agreement.
+
+#endregion
+
+#region Imports
+
+using System.Data.Entity.Migrations;
+
+#endregion
+
+namespace vidly_aspnet_learn.Migrations
+{
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 0, 0, 0)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 30, 1, 10)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 90, 3, 15)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4, 300, 12, 20)");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
