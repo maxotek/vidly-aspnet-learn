19fff5bfdb5a943f29eab44ae7f6f2e51d901bf4
maxotek <partho@maxotek.com>
2016-08-03 01:44:13 +0530

    Used bootbox to render bootstrap confirmation dialog during deletion

--- vidly-aspnet-learn/App_Start/BundleConfig.cs
+++ vidly-aspnet-learn/App_Start/BundleConfig.cs
@@ -37,6 +37,7 @@ namespace vidly_aspnet_learn
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                 "~/Scripts/bootstrap.js",
+                "~/Scripts/bootbox.js",
                 "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
